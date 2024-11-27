7b609e5c42673f3b7340eeddf5b4ec542a891280
shandysatria <shandy@99.co>
2024-11-27 18:15:05 +0700

    Menambahkan efek scroll untuk mengganti banner

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ document.addEventListener("scroll", function() {
     let scrollPosition = window.scrollY;
     let heroSection = document.querySelector(".hero");
 
-    // Tentukan perubahan gambar banner berdasarkan scroll
+    // Tentukan perubahan gambar berdasarkan scroll
     if (scrollPosition < 500) {
-        heroSection.style.backgroundImage = "url('images/banner-1.png')";
+        heroSection.style.backgroundImage = "url('images/banner1.png')";
     } else if (scrollPosition >= 500 && scrollPosition < 1000) {
-        heroSection.style.backgroundImage = "url('images/banner-2.png')";
+        heroSection.style.backgroundImage = "url('images/banner2.png')";
     } else if (scrollPosition >= 1000) {
-        heroSection.style.backgroundImage = "url('images/banner-3.png')";
+        heroSection.style.backgroundImage = "url('images/banner3.png')";
     }
 });
