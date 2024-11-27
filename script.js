4a028ee2313144f2fd5500b80333e4d2f735a6ce
shandysatria <shandy@99.co>
2024-11-27 21:46:59 +0700

    Menambahkan efek scroll untuk mengganti banner

--- script.js
+++ script.js
@@ -2,36 +2,29 @@ const heroSections = document.querySelectorAll('.hero-section');
 const dots = document.querySelectorAll('.dot');
 let currentSectionIndex = 0;
 
-// Tambahkan event listener untuk setiap titik navigasi
 dots.forEach((dot, index) => {
   dot.addEventListener('click', () => {
-    goToSection(index);
-  });
-  dot.setAttribute('aria-label', `Go to section ${index + 1}`);
-});
-
-// Fungsi untuk berpindah ke section
-function goToSection(index) {
-  if (index >= 0 && index < heroSections.length) {
     currentSectionIndex = index;
     scrollToSection(currentSectionIndex);
+  });
+  dot.setAttribute('aria-label', `Go to section ${index + 1}`); // Added aria-label
+});
+
+function scrollToSection(index) {
+  if (index >= 0 && index < heroSections.length) {
+    heroSections[index].scrollIntoView({ behavior: 'smooth' });
+    updateActiveDot();
   }
 }
 
-// Fungsi untuk melakukan scroll ke section
-function scrollToSection(index) {
-  heroSections[index].scrollIntoView({ behavior: 'smooth' });
-  updateActiveDot();
-}
-
-// Fungsi untuk memperbarui titik navigasi aktif
 function updateActiveDot() {
   dots.forEach((dot, index) => {
     dot.classList.toggle('active', index === currentSectionIndex);
   });
 }
 
-// Event listener untuk scroll mouse wheel
-window.addEventListener('wheel', (event) => {
-  goToSection((currentSectionIndex + (event.deltaY > 0 ? 1 : -1) + heroSections.length) % heroSections.length);
+
+window.addEventListener('wheel', function(event) {
+  currentSectionIndex = event.deltaY > 0 ? (currentSectionIndex + 1) % heroSections.length : (currentSectionIndex - 1 + heroSections.length) % heroSections.length;
+  scrollToSection(currentSectionIndex);
 });
