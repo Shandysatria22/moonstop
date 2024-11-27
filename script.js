e6a44ed50d6b1ec712fa932f37de7e56f5f975f6
shandysatria <shandy@99.co>
2024-11-27 15:33:57 +0700

    Menambahkan struktur dasar dan styling

---
+++ script.js
@@ -0,0 +1,10 @@
+// Script untuk membuat efek scroll
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
